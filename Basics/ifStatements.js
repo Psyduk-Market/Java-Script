b00b7c9ada92f3b0a9bc57fd0ca6978cf9f9e5c2
Thyme's Garden <cnawaphanarat@gmail.com>
2019-08-08 09:55:30 +1200

    Add files via upload

--- Basics/ifStatements.js
+++ Basics/ifStatements.js
@@ -2,10 +2,13 @@ var fizzBuzz = function(number) {
 
     if ((number % 3 == 0) && (number % 5 == 0)){
         console.log("FizzBuzz");
+        return "FizzBuzz";
     } else if (number % 5 == 0){
         console.log("Buzz");
+        return "Buzz";
     } else if (number % 3 == 0) {
         console.log("Fizz");
+        return "Fizz";
     }
 
 };
--- Basics/switchStatements.js
+++ Basics/switchStatements.js
@@ -2,12 +2,15 @@ var fizzBuzz = function(number) {
     switch (true) {
         case ((number % 3 == 0) && (number % 5 == 0)):
             console.log("FizzBuzz");
+            return "FizzBuzz";
             break;
         case (number % 3 == 0):
             console.log("Fizz");
+            return "Fizz";
             break;
         case (number % 5 == 0):
             console.log("Buzz");
+            return "Buzz";
             break;
         default:
             // console.log("Unknown weather type!");
